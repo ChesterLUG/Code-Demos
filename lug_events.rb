6e084faf2f330bb385a7c9695a704990a8762cf7
Ben Arnold <ben@seawolfsanctuary.com>
2013-06-06 23:48:44 +0100

    Add: example classes of LUG events

---
+++ lug_events.rb
@@ -0,0 +1,16 @@
+# This is GPL licensed. Because you really want this...
+
+class LUG
+  attr_reader :location
+end
+
+class Presentation
+end
+
+class Event
+  attr_reader :date, :title, :trip?
+end
+
+class Person
+  attr_reader :arms, :legs
+end
